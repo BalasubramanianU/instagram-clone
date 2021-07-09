417794be7cd504a996e390083284a8ef7e1ccc4a
Balasubramanian U <ubalasubramanian03@gmail.com>
2021-07-09 19:10:45 +0530

    Added login feature positive scenario (with testing) and password hashing

--- backend/__tests__/integration tests/routes/user.js
+++ backend/__tests__/integration tests/routes/user.js
@@ -4,44 +4,134 @@ const server = require("../../../index");
 const { generateJwtToken } = require("../../../routes/user");
 const { UserWithEmail, UserWithNumber } = require("../../../models/user");
 
-const payloadWithEmail = {
-  email: "bala@gmail.com",
-  fullName: "bala",
-  userName: "__bala__",
-  password: "abc@123",
-};
-describe("signup with email", () => {
-  afterEach(async () => {
-    await UserWithEmail.remove(payloadWithEmail);
-    await server.close();
+describe("signup", () => {
+  const payloadWithEmail = {
+    email: "test123@gmail.com",
+    fullName: "test",
+    userName: "__test_",
+    password: "abc@123",
+  };
+
+  describe("signup with email", () => {
+    afterEach(async () => {
+      await UserWithEmail.deleteOne(payloadWithEmail);
+      await server.close();
+    });
+
+    it("checks if it returns 200 and returns a jwt auth header if everything is ok", async () => {
+      const token = generateJwtToken(payloadWithEmail);
+      const res = await request(server)
+        .post("/user/signup")
+        .send(payloadWithEmail);
+
+      expect(res.status).toBe(200);
+      expect(res.header).toHaveProperty("x-auth-header", token);
+    });
   });
 
-  const token = generateJwtToken(payloadWithEmail);
+  const payloadWithNumber = {
+    mobileNumber: "123456",
+    fullName: "test",
+    userName: "_test_",
+    password: "abc@123",
+  };
 
-  it("checks if it returns 200 and returns a jwt if everything is ok", async () => {
-    const res = await request(server).post("/user/").send(payloadWithEmail);
-    expect(res.status).toBe(200);
-    expect(res.text).toMatch(token);
+  describe("signup with mobile number", () => {
+    afterEach(async () => {
+      await UserWithNumber.deleteOne(payloadWithNumber);
+      await server.close();
+    });
+
+    it("checks if it returns 200 and returns a jwt auth header if everything is ok", async () => {
+      const token = generateJwtToken(payloadWithNumber);
+      const res = await request(server)
+        .post("/user/signup")
+        .send(payloadWithNumber);
+
+      expect(res.status).toBe(200);
+      expect(res.header).toHaveProperty("x-auth-header", token);
+    });
   });
 });
 
-const payloadWithNumber = {
-  mobileNumber: "123456",
-  fullName: "bala",
-  userName: "__bala__",
-  password: "abc@123",
-};
-describe("signup with mobile number", () => {
-  afterEach(async () => {
-    await UserWithNumber.remove(payloadWithNumber);
-    await server.close();
+describe("login", () => {
+  const payloadWithEmail = {
+    email: "test@gmail.com",
+    password: "abc@123",
+  };
+
+  describe("login with email", () => {
+    afterEach(async () => {
+      await server.close();
+    });
+
+    it("checks if it returns 200 and returns a jwt auth header if everything is ok", async () => {
+      const token = generateJwtToken(payloadWithEmail);
+      const res = await request(server)
+        .post("/user/login")
+        .send(payloadWithEmail);
+
+      expect(res.status).toBe(200);
+      expect(res.header).toHaveProperty("x-auth-header", token);
+    });
   });
 
-  const token = generateJwtToken(payloadWithNumber);
+  const payloadWithNumber = {
+    mobileNumber: "12345678",
+    password: "abc@123",
+  };
 
-  it("checks if it returns 200 and returns a jwt if everything is ok", async () => {
-    const res = await request(server).post("/user/").send(payloadWithNumber);
-    expect(res.status).toBe(200);
-    expect(res.text).toMatch(token);
+  describe("login with mobile number", () => {
+    afterEach(async () => {
+      await server.close();
+    });
+    it("checks if it returns 200 and returns a jwt auth header if everything is ok", async () => {
+      const token = generateJwtToken(payloadWithNumber);
+      const res = await request(server)
+        .post("/user/login")
+        .send(payloadWithNumber);
+
+      expect(res.status).toBe(200);
+      expect(res.header).toHaveProperty("x-auth-header", token);
+    });
+  });
+
+  const nameInsideMobileNo = {
+    userName: "__test__",
+    password: "abc@123",
+  };
+
+  describe("login with user name inside mobileNumber collections in mongodb", () => {
+    afterEach(async () => {
+      await server.close();
+    });
+    it("checks if it returns 200 and returns a jwt auth header if everything is ok", async () => {
+      const token = generateJwtToken(nameInsideMobileNo);
+      const res = await request(server)
+        .post("/user/login")
+        .send(nameInsideMobileNo);
+
+      expect(res.status).toBe(200);
+      expect(res.header).toHaveProperty("x-auth-header", token);
+    });
+  });
+  const nameInsideEmail = {
+    userName: "_test__",
+    password: "abc@123",
+  };
+
+  describe("login with user name inside mobileNumber collections in mongodb", () => {
+    afterEach(async () => {
+      await server.close();
+    });
+    it("checks if it returns 200 and returns a jwt auth header if everything is ok", async () => {
+      const token = generateJwtToken(nameInsideEmail);
+      const res = await request(server)
+        .post("/user/login")
+        .send(nameInsideEmail);
+
+      expect(res.status).toBe(200);
+      expect(res.header).toHaveProperty("x-auth-header", token);
+    });
   });
 });
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { UserWithEmail, UserWithNumber } = require("../models/user");
 
+const bcrypt = require("bcrypt");
 const config = require("config");
 const jwt = require("jsonwebtoken");
 
@@ -10,13 +11,49 @@ function generateJwtToken(payload) {
   return jwt.sign(payload, config.get("jwtKey"));
 }
 
-router.post("/", async (req, res) => {
+async function passHash(password) {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+}
+
+router.post("/login", async (req, res) => {
+  let user;
+  if (req.body.email)
+    user = await UserWithEmail.findOne({ email: req.body.email });
+
+  if (req.body.mobileNumber)
+    user = await UserWithNumber.findOne({
+      mobileNumber: req.body.mobileNumber,
+    });
+
+  if (!req.body.email && !req.body.mobileNumber) {
+    const userName = await UserWithEmail.findOne({
+      userName: req.body.userName,
+    });
+
+    if (userName) user = userName;
+    else user = await UserWithNumber.findOne({ userName: req.body.userName });
+  }
+
+  const token = generateJwtToken(req.body);
+  res.status(200).header("x-auth-header", token).send(user);
+});
+
+router.post("/signup", async (req, res) => {
   let user = req.body.email
     ? new UserWithEmail(req.body)
     : new UserWithNumber(req.body);
-  await user.save();
+  let result;
+  if (
+    !(await UserWithEmail.findOne({ userName: user.userName })) &&
+    !(await UserWithNumber.findOne({ userName: user.userName }))
+  ) {
+    user.password = await passHash(user.password);
+    await user.save();
+  }
+
   const token = generateJwtToken(req.body);
-  res.status(200).send(token);
+  res.status(200).header("x-auth-header", token).send();
 });
 
 module.exports = { router, generateJwtToken };
